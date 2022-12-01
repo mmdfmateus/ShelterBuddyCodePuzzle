378352a7c262d74ea35e4526785cf2cae357ab2e
Mateus Mourão <mmdfmateus@gmail.com>
2022-12-01 00:00:14 -0300

    change repository from scoped to singleton

--- ShelterBuddy.CodePuzzle.Api/Program.cs
+++ ShelterBuddy.CodePuzzle.Api/Program.cs
@@ -5,7 +5,7 @@ using ShelterBuddy.CodePuzzle.Core.Entities;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped<IRepository<Animal, Guid>, AnimalRepository>();
+builder.Services.AddSingleton<IRepository<Animal, Guid>, AnimalRepository>();
 builder.Services.AddScoped<AnimalModelValidator>();
 
 builder.Services.AddControllers();
